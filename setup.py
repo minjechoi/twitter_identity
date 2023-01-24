8796d7fdfc7e4adeabb4c60adf89eb7bb999c644
minjechoi <devnote5676@gmail.com>
2023-01-24 22:40:48 +0000

    Update setup.py to enable pip packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = 'twitter_identity',
+    version = "0.0.1",
+    packages=['twitter_identity'],
+    url="",
+    install_requires=[],
+    py_modules=[]
+    
+)
