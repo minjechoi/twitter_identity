70ce7424b2e5f2a998deec3921f318d25c12119f
minjechoi <devnote5676@gmail.com>
2023-01-29 06:45:00 +0000

    Add code for cleaning tweets

--- twitter_identity/utils/utils.py
+++ twitter_identity/utils/utils.py
@@ -1,9 +1,11 @@
 import os
 from datetime import datetime
-from dateutil.parser import parse
 import logging
+import re
+from typing import Literal
 
 import numpy as np
+from dateutil.parser import parse
 
 def get_weekly_bins(timestamp):
     """
@@ -19,6 +21,46 @@ def get_weekly_bins(timestamp):
         
     diff = dt_current - dt_base
     return diff.days / 7
+
+_STRIP_TYPES = Literal[None, 'replace', 'remove']
+def strip_tweet(text, url: _STRIP_TYPES='remove', username: _STRIP_TYPES='replace', hashtag: _STRIP_TYPES=None, non_ascii=True, lowercase=False):
+    """Function that strips certain parts from text
+
+    Args:
+        text (str): Input string
+        url (_STRIP_TYPES): _description_
+        username (_STRIP_TYPES): _description_
+    """
+    # remove potential linebreaks and tabs
+    text = ' '.join(text.replace('\n',' ').replace('\t',' ').split()).strip()
+    
+    # remove other languages
+    if non_ascii:
+        text = text.encode('ascii', errors='ignore').decode()
+    
+    # lowercase
+    if lowercase:
+        text = text.lower()
+    
+    # remove URLs
+    if url=='replace':
+        text = re.sub(r'http\S+', 'URL', text).strip()
+    elif url=='remove':
+        text = ' '.join(re.sub(r'http\S+', '', text).split()).strip()
+    
+    # remove usernames
+    if username=='replace':
+        text = re.sub(r'@\w+', '@username', text).strip()
+    elif username=='remove':
+        text = ' '.join(re.sub(r'@\w+', '', text).strip().split()).strip()
+    
+    # remove hashtags
+    if hashtag=='replace':
+        text = re.sub(r'#\w+', '#hashtag', text).strip()
+    elif hashtag=='remove':
+        text = ' '.join(re.sub(r'#\w+', '', text).strip().split()).strip()
+    
+    return text
 
 def write_data_file_info(script_directory, function_name, output_file,  input_files=None, log_file='/home/minje/projects/twitter_identity/logs/log_files.tsv'
     ):
@@ -45,5 +87,7 @@ def test_write_data_file_info():
 
 if __name__=='__main__':
     # get_weekly_bins()
-    print(test_write_data_file_info())
+    # print(test_write_data_file_info())
+    text = "is it just me or does anyone else feel happy asf when you get a dm from someone saying \u201cyou\u201d or \u201cthis is you\u201d like damn bitch you thought of me\ud83e\udd7a\ud83e\udd7a\ud83e\udd7a\ud83e\udd7a\ud83e\udd7a"
+    print(strip_tweet(text))
     
