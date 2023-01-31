7cb3015011866af3843a408281c62547689a89ff
minjechoi <devnote5676@gmail.com>
2023-01-31 20:58:57 +0000

    Update fixes to ensure balanced training sets

--- twitter_identity/data_preprocessing/combine_files.py
+++ twitter_identity/data_preprocessing/combine_files.py
@@ -8,6 +8,7 @@ import re
 from multiprocessing import Pool
 from random import sample, shuffle
 
+from sklearn.utils import resample
 import pandas as pd
 import ujson as json
 from tqdm import tqdm
@@ -197,7 +198,7 @@ def merge_training_sets_worker(tweet_type, identity, user_id_file, load_dir, aux
             while(len(texts)>=n_tweets_per_sample):
                 line = ' '.join(texts[:n_tweets_per_sample])
                 f.write(f'{uid}\t{label}\t{line}\n')
-                texts = texts[n_tweets_per_sample:]    
+                texts = texts[n_tweets_per_sample:]
     return
 
 def merge_training_sets(user_id_file, load_dir, aux_load_dir, save_dir, n_tweets_per_sample=5, max_samples_per_user=5):
@@ -225,49 +226,101 @@ def merge_training_sets(user_id_file, load_dir, aux_load_dir, save_dir, n_tweets
     write_data_file_info(__file__,merge_training_sets.__name__,save_dir,[load_dir])
     return
 
-def split_data_train_test_val_worker(load_file, load_dir, save_dir):
+def split_data_train_test_val_worker(load_file, load_dir, save_dir, n_train_samples=50000):
     # get all tweets for each user so that the same user doesn't appear in multiple splits
-    user2data = {}
+    file_prefix = '.'.join(load_file.split('.')[:2]) # prefix for saving file
+    
+    # upsampled to 100K (80K/10K/10K) samples, balanced between two sets
+    label2data = {'0':{}, '1':{}} # maps the tweets of all user by label
+    # all_users = {} # users for train/test mapping
+    
+    # load all data
     with gzip.open(join(load_dir,load_file),'rt') as f:
         for line in f:
             uid,label,text=line.split('\t')
-            if uid not in user2data:
-                user2data[uid]=[]
-            user2data[uid].append(line)
+            if uid not in label2data[label]:
+                label2data[label][uid]=[]
+            label2data[label][uid].append(line)
+            
+    # split users into train/test/val
+    user_assignment = {
+        '1':{'train':[], 'val':[], 'test':[]},
+        '0':{'train':[], 'val':[], 'test':[]}
+        }
+    for label,D in label2data.items():
+        uids=list(D.keys())
+        shuffle(uids)
+        ln=int(0.1*len(uids))
+        user_assignment[label]['test']=uids[:ln]
+        user_assignment[label]['val']=uids[ln:ln*2]
+        user_assignment[label]['train']=uids[ln*2:]
     
-    # assign users to train/test/val
-    uids = list(user2data.keys())
-    shuffle(uids)
+    split2count = {}
     
-    file_prefix = '.'.join(load_file.split('.')[:2])
-    ln1 = int(len(uids)*0.1)
-    ln2 = int(len(uids)*0.2)
-    
-    with gzip.open(join(save_dir,f'{file_prefix}.val.tsv.gz'),'wt') as f:
-        for uid in uids[:ln1]:
-            lines = user2data[uid]
-            for line in lines:
+    # get tweet data for train/test/val
+    for split in ['test','val']:
+        all_samples = []
+        for label in ['0','1']:
+            split_samples = []
+            uids = user_assignment[label][split]
+            for uid in uids:
+                split_samples.extend(label2data[label][uid]) # append all tweets by this user
+            split2count[(split,label)]=len(split_samples)
+            all_samples.extend(split_samples)
+
+        # shuffle(all_samples)
+        # print(split,label,len(all_samples))
+        with gzip.open(join(save_dir,f'{file_prefix}.{split}.tsv.gz'),'wt') as f:
+            for line in all_samples:
                 f.write(line)
-    print(f"Wrote val set for {file_prefix}")
-    with gzip.open(join(save_dir,f'{file_prefix}.test.tsv.gz'),'wt') as f:
-        for uid in uids[ln1:ln2]:
-            lines = user2data[uid]
-            for line in lines:
-                f.write(line)
-    print(f"Wrote test set for {file_prefix}")
+                
+    # we have to match number of lines for train
+    all_samples = []
+    for label,D in user_assignment.items():
+        split_samples = []
+        uids = D['train']
+        if len(uids)>=n_train_samples: # if uids>=train samples required, we just use one sample per uid
+            uids2 = resample(uids,replace=False,n_samples=n_train_samples)
+            for uid in uids2:
+                split_samples.append(label2data[label][uid][0])
+        else: # we augment one per user and check if we get over the limit
+            S=set(uids)
+            while(len(S)>0):
+                S2 = set() # for next round
+                uids=list(S)
+                for uid in uids:
+                    if len(split_samples)>=n_train_samples:
+                        break
+                    else:
+                        split_samples.append(label2data[label][uid][0]) # add sample
+                        label2data[label][uid].pop(0) # pop first item
+                        if len(label2data[label][uid])>0:
+                            S2.add(uid)
+                if len(split_samples)>=n_train_samples:
+                    break
+                S = S2 # update valid users for next round
+            if len(split_samples)>=n_train_samples:
+                split_samples = split_samples[:n_train_samples]
+            else:
+                split_samples = resample(split_samples, replace=True, n_samples=n_train_samples)
+
+        all_samples.extend(split_samples) # add to entire set
+        split2count[('train',label)]=len(split_samples)
+
+    print(file_prefix,split2count)
     with gzip.open(join(save_dir,f'{file_prefix}.train.tsv.gz'),'wt') as f:
-        for uid in uids[ln2:]:
-            lines = user2data[uid]
-            for line in lines:
-                f.write(line)
+        for line in all_samples:
+            f.write(line)
+    
     print(f"Wrote train set for {file_prefix}")
     return
 
-def split_data_train_test_val(load_dir, save_dir):
+def split_data_train_test_val(load_dir, save_dir, n_train_samples=50000):
     files = sorted(os.listdir(load_dir))
-    inputs = [(file,load_dir,save_dir) for file in files]
+    inputs = [(file,load_dir,save_dir, n_train_samples) for file in files]
     pool = Pool(30)
     pool.starmap(split_data_train_test_val_worker, inputs)
+    # split_data_train_test_val_worker(*inputs[0])
     write_data_file_info(__file__,split_data_train_test_val.__name__,save_dir,[load_dir])
     return
 
@@ -287,4 +340,4 @@ if __name__=='__main__':
     # split into train/test/val splits
     load_dir = '/shared/3/projects/bio-change/data/interim/identity_classifier-train_data/train_data/5_tweets_per_sample-5_max_samples_per_user'
     save_dir = '/shared/3/projects/bio-change/data/processed/identity_classifier-train_data'
-    split_data_train_test_val(load_dir, save_dir)
+    split_data_train_test_val(load_dir, save_dir, 50000)
