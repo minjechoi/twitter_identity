e78b528358ac3225618a863513bab0349f7cd472
minjechoi <devnote5676@gmail.com>
2023-02-03 02:24:37 +0000

    Update functions

--- twitter_identity/data_preprocessing/create_matching_data.py
+++ twitter_identity/data_preprocessing/create_matching_data.py
@@ -4,8 +4,9 @@ import gzip
 
 import pandas as pd
 import ujson as json
+from tqdm import tqdm
 
-from twitter_identity.utils.utils import get_weekly_bins
+from twitter_identity.utils.utils import get_weekly_bins,write_data_file_info
 
 def load_uids(treat_user_file, control_user_file):
     """_summary_
@@ -37,7 +38,7 @@ def obtain_profile_features(uids, user_info_file, save_dir):
         save_dir (_type_): _description_
     """
     uid2info = {uid:None for uid in uids}
-    print(len(uids))
+    print(f'{len(uids)} candidate users!')
     with gzip.open(user_info_file,'rt') as f:
         for line in f:
             obj = json.loads(line)
@@ -50,12 +51,13 @@ def obtain_profile_features(uids, user_info_file, save_dir):
                     'created_at':obj['created_at'],
                     }
     out = []
-    for uid,V in uid2info.items():
+    for uid,V in tqdm(uid2info.items()):
         if V is not None:
             out.append((uid, V['fri'], V['fol'], V['sta'], V['created_at']))
     df=pd.DataFrame(out,columns=['user_id','fri','fol','sta','created_at'])
     save_file = join(save_dir,'user_activity_features.df.tsv')
     df.to_csv(save_file,sep='\t',index=False)
+    write_data_file_info(__file__, obtain_profile_features.__name__, save_file, [user_info_file])
     print(df.shape)
     print(f'Saved to {save_file}')
     return
@@ -77,13 +79,14 @@ def obtain_description_features(uids, desc_file, save_dir):
                 uid2info[uid].append((float(dt),desc))
     
     out = []
-    for uid,V in uid2info.items():
+    for uid,V in tqdm(uid2info.items()):
         assert len(V)==2
         dt,desc = V[1][0], V[0][1]
         week = get_weekly_bins(dt)
         out.append((uid,dt,week,desc))
     df=pd.DataFrame(out,columns=['user_id','timestamp_treated','week_treated','profile_before_update'])
     save_file = join(save_dir,'description_features.df.tsv')
+    write_data_file_info(__file__, obtain_description_features.__name__, save_file, [desc_file])
     df.to_csv(save_file,sep='\t',index=False)
     print(df.shape)
     print(f'Saved to {save_file}')
@@ -93,10 +96,10 @@ if __name__=='__main__':
     treat_user_file = '/shared/3/projects/bio-change/data/interim/treated-control-users/all_treated_users.tsv'
     control_user_file = '/shared/3/projects/bio-change/data/interim/treated-control-users/all_potential_control_users.tsv'
     save_dir = '/shared/3/projects/bio-change/data/interim/propensity-score-matching'
-    user_info_file = '/shared/3/projects/bio-change/data/raw/user_info/user_profile-2020.04.json.gz'
+    user_info_file = '/shared/3/projects/bio-change/data/external/treated-control-tweets/all_user_profiles.json.gz'
     desc_info_file = '/shared/3/projects/bio-change/data/interim/description_changes/filtered/description_changes_1plus_changes.tsv.gz'
 
     valid_users = load_uids(treat_user_file, control_user_file)
-    obtain_profile_features(valid_users, user_info_file, save_dir)
+    # obtain_profile_features(valid_users, user_info_file, save_dir)
     obtain_description_features(valid_users, desc_info_file, save_dir)
     
