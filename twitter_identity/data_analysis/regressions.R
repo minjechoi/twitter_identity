b9e65e5da80d63bb33aa45bbf298ba425c302200
minjechoi <devnote5676@gmail.com>
2023-05-08 23:56:04 +0000

    Upload R code

---
+++ twitter_identity/data_analysis/regressions.R
@@ -0,0 +1,12 @@
+library(DRDID)
+# Load data in long format that comes in the DRDID package
+data(nsw_long)
+# Form the Lalonde sample with CPS comparison group
+eval_lalonde_cps <- subset(nsw_long, nsw_long$treated == 0 | nsw_long$sample == 2)
+# Now, to estimate the ATT using the Improved Locally Efficient Doubly Robust DID estimator, we can use the drdid function:
+
+# Implement improved locally efficient DR DID:
+out <- drdid(yname = "re", tname = "year", idname = "id", dname = "experimental",
+             xformla= ~ age + educ + black + married + nodegree + hisp + re74,
+             data = eval_lalonde_cps, panel = TRUE)
+summary(out)
